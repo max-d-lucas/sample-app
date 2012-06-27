ca2bcd3ee1429b46d48d9068efc6aba535a9ca8c
Max Lucas <max.d.lucas@gmail.com>
2012-06-27 16:18:30 -0500

    Finished sign in

--- Gemfile
+++ Gemfile
@@ -12,6 +12,8 @@ group :development, :test do
   gem 'spork', '0.9.0'
   gem 'annotate', '~> 2.4.1.beta'
   gem 'factory_girl_rails', '1.4.0'
+  gem 'cucumber-rails', '1.2.1', require: false
+  gem 'database_cleaner', '0.7.0'  
 end
 
 # Gems used only for assets and not required
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
 
     before_save { |user| user.email = email.downcase }
     before_validation { |user| user.email = email.downcase }
+    before_save :create_remember_token
 
     VALID_EMAIL_REGEX = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i
     validates :name, presence: true, length: {maximum:255, minimum:3}
@@ -12,19 +13,23 @@ class User < ActiveRecord::Base
     validates :password_confirmation, presence: true
 
     validate :meets_secure_password_rules, :meets_name_rules
+    private
 
-    def meets_secure_password_rules
-        errors.add(:password, "can not be the same as your email address.") if password.downcase==email.downcase
-        errors.add(:password, "can not be the same as your name.") if password.downcase==name.downcase
-        errors.add(:password, "Password must have at least 1 uppercase character.") unless password.match(/[[:upper:]]+?/)
-        errors.add(:password, "Password must have at least 1 lowercase character.") unless password.match(/[[:lower:]]+?/)
-        errors.add(:password, "Password must have at least 1 numeric character.") unless password.match(/[[:digit:]]+?/)
-        password.match(/[\W]+?/) {|schar| errors.add(:password, "Password may not have special characters, " + schar.to_s + " found" ) }
-    end  
+        def create_remember_token
+          self.remember_token = SecureRandom.urlsafe_base64
+        end
+        def meets_secure_password_rules
+            errors.add(:password, "can not be the same as your email address.") if password.downcase==email.downcase
+            errors.add(:password, "can not be the same as your name.") if password.downcase==name.downcase
+            errors.add(:password, "Password must have at least 1 uppercase character.") unless password.match(/[[:upper:]]+?/)
+            errors.add(:password, "Password must have at least 1 lowercase character.") unless password.match(/[[:lower:]]+?/)
+            errors.add(:password, "Password must have at least 1 numeric character.") unless password.match(/[[:digit:]]+?/)
+            password.match(/[\W]+?/) {|schar| errors.add(:password, "Password may not have special characters, " + schar.to_s + " found" ) }
+        end  
 
-    def meets_name_rules
-        name.match(/[^[:alnum:][:blank:]\_~\[\]\?!\(\)\.\-'@]+?/) {|schar| errors.add(:name, "may not have special characters, " + schar.to_s + " found" ) }
-    end    
+        def meets_name_rules
+            name.match(/[^[:alnum:][:blank:]\_~\[\]\?!\(\)\.\-'@]+?/) {|schar| errors.add(:name, "may not have special characters, " + schar.to_s + " found" ) }
+        end    
 end
 # == Schema Information
 #
